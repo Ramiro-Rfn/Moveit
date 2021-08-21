50842a1f3a137aba9af226167633a3392b37ee60
Ramiro_Nzau <ramirofrancisco962014@gmail.com>
2021-08-21 09:48:29 +0100

    inicial commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+     <h1>Hello next
+     </h1>
+    </div>
+  )
+}

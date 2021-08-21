0ed5cd3a517efd40eec6b6093714994fca57d459
Ramiro_Nzau <ramirofrancisco962014@gmail.com>
2021-08-21 10:46:47 +0100

    criando o componente de experienceBar

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { ExperienceBar } from '../components/ExpirienceBar'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar/>
+    </div>
+  )
+}
